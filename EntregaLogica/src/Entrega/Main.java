0b8799b4d889ab3928bbea85e876e567335f867e
estebantorresrd05 <estebantorresrd0506@gmail.com>
2026-02-10 09:50:57 -0500

    Se agrega eliminar o agregar cursos

--- EntregaLogica/src/Entrega/Main.java
+++ EntregaLogica/src/Entrega/Main.java
@@ -9,184 +9,224 @@ public class Main {
 
         String[] nombres = new String[10];
         String[] correos = new String[10];
-        String[] contrasenas = new String[10];
-        String[] cursos = new String[10];
+        String[] claves = new String[10];
+        String[] cursosUsuario = new String[10];
 
-        int contadorUsuarios = 0;
+        String[] cursos = new String[10];
+        double[] precios = new double[10];
+
+        int totalUsuarios = 0;
+        int totalCursos = 3;
         int usuarioActual = -1;
         int opcion = -1;
-        int elegir;
-        int borrar;
+
+        boolean sesion = false;
+        boolean admin = false;
 
         String correo;
-        String pass;
+        String clave;
         String r;
-        String salir;
 
         String correoAdmin = "admin@cursos.com";
-        String passAdmin = "admin123";
+        String claveAdmin = "admin123";
 
-        boolean sesionIniciada = false;
-        boolean esAdmin = false;
+        cursos[0] = "Desarrollo Web";
+        cursos[1] = "JavaScript Moderno";
+        cursos[2] = "CSS Avanzado";
 
-        String titulo1 = "Desarrollo Web";
-        String titulo2 = "JavaScript Moderno";
-        String titulo3 = "CSS Avanzado";
-
-        double precio1 = 159990;
-        double precio2 = 179990;
-        double precio3 = 149990;
+        precios[0] = 159990;
+        precios[1] = 179990;
+        precios[2] = 149990;
 
         do {
-            System.out.println("\n===== PLATAFORMA DE CURSOS =====");
+            System.out.println("\n=== PLATAFORMA DE CURSOS ===");
 
-            if (!sesionIniciada) {
+            if (!sesion) {
                 System.out.println("1. Registrarse");
                 System.out.println("2. Iniciar sesión");
                 System.out.println("0. Salir");
-            } else if (esAdmin) {
+            } else if (admin) {
                 System.out.println("1. Ver usuarios");
-                System.out.println("2. Modificar precios");
-                System.out.println("3. Borrar usuario");
-                System.out.println("4. Cerrar sesión");
+                System.out.println("2. Cambiar precios");
+                System.out.println("3. Eliminar usuario");
+                System.out.println("4. Agregar curso");
+                System.out.println("5. Eliminar curso");
+                System.out.println("6. Cerrar sesión");
                 System.out.println("0. Salir");
             } else {
-                System.out.println("1. Ver cursos y elegir");
+                System.out.println("1. Ver cursos");
                 System.out.println("2. Comprar curso");
                 System.out.println("3. Ver perfil");
                 System.out.println("4. Cerrar sesión");
                 System.out.println("0. Salir");
             }
 
-            System.out.print("Elige una opción: ");
             opcion = sc.nextInt();
             sc.nextLine();
 
-            if (opcion == 1 && !sesionIniciada) {
-                System.out.print("Nombre: ");
-                nombres[contadorUsuarios] = sc.nextLine();
-                System.out.print("Correo: ");
-                correos[contadorUsuarios] = sc.nextLine();
-                System.out.print("Contraseña: ");
-                contrasenas[contadorUsuarios] = sc.nextLine();
-                cursos[contadorUsuarios] = "Ninguno";
-                contadorUsuarios++;
-                System.out.println("Registro exitoso.");
-            }
+            switch (opcion) {
 
-            else if (opcion == 2 && !sesionIniciada) {
-                System.out.print("Correo: ");
-                correo = sc.nextLine();
-                System.out.print("Contraseña: ");
-                pass = sc.nextLine();
+                case 1:
+                    if (!sesion) {
+                        System.out.print("Nombre: ");
+                        nombres[totalUsuarios] = sc.nextLine();
+                        System.out.print("Correo: ");
+                        correos[totalUsuarios] = sc.nextLine();
+                        System.out.print("Contraseña: ");
+                        claves[totalUsuarios] = sc.nextLine();
+                        cursosUsuario[totalUsuarios] = "Ninguno";
+                        totalUsuarios++;
+                        System.out.println("Registro exitoso.");
+                    } else if (admin) {
+                        for (int i = 0; i < totalUsuarios; i++) {
+                            System.out.println((i + 1) + ". " + nombres[i] + " | " + correos[i] + " | " + cursosUsuario[i]);
+                        }
+                    } else {
+                        for (int i = 0; i < totalCursos; i++) {
+                            System.out.println((i + 1) + ". " + cursos[i] + " $" + precios[i]);
+                        }
+                        System.out.print("Seleccione un curso: ");
+                        int c = sc.nextInt();
+                        sc.nextLine();
 
-                if (correo.equals(correoAdmin) && pass.equals(passAdmin)) {
-                    sesionIniciada = true;
-                    esAdmin = true;
-                    System.out.println("Bienvenido ADMINISTRADOR");
-                } else {
-                    boolean encontrado = false;
-                    for (int i = 0; i < contadorUsuarios; i++) {
-                        if (correo.equals(correos[i]) && pass.equals(contrasenas[i])) {
-                            sesionIniciada = true;
-                            usuarioActual = i;
-                            encontrado = true;
-                            System.out.println("Bienvenido " + nombres[i]);
-                            break;
+                        if (c > 0 && c <= totalCursos) {
+                            cursosUsuario[usuarioActual] = cursos[c - 1];
+                            System.out.println("Curso seleccionado.");
                         }
                     }
-                    if (!encontrado) {
-                        System.out.println("Datos incorrectos.");
+                    break;
+
+                case 2:
+                    if (!sesion) {
+                        System.out.print("Correo: ");
+                        correo = sc.nextLine();
+                        System.out.print("Contraseña: ");
+                        clave = sc.nextLine();
+
+                        if (correo.equals(correoAdmin) && clave.equals(claveAdmin)) {
+                            sesion = true;
+                            admin = true;
+                            System.out.println("Bienvenido ADMIN.");
+                        } else {
+                            boolean encontrado = false;
+                            for (int i = 0; i < totalUsuarios; i++) {
+                                if (correo.equals(correos[i]) && clave.equals(claves[i])) {
+                                    sesion = true;
+                                    usuarioActual = i;
+                                    encontrado = true;
+                                    System.out.println("Bienvenido " + nombres[i]);
+                                    break;
+                                }
+                            }
+                            if (!encontrado) {
+                                System.out.println("Datos incorrectos.");
+                            }
+                        }
+                    } else if (admin) {
+                        for (int i = 0; i < totalCursos; i++) {
+                            System.out.println((i + 1) + ". " + cursos[i] + " $" + precios[i]);
+                        }
+
+                        System.out.print("Curso a modificar: ");
+                        int c = sc.nextInt();
+                        sc.nextLine();
+
+                        if (c > 0 && c <= totalCursos) {
+                            System.out.print("Nuevo precio: ");
+                            precios[c - 1] = sc.nextDouble();
+                            sc.nextLine();
+                            System.out.println("Precio actualizado.");
+                        }
+                    } else {
+                        if (!cursosUsuario[usuarioActual].equals("Ninguno")) {
+                            System.out.print("¿Desea comprar el curso? (s/n): ");
+                            r = sc.nextLine();
+                            if (r.equalsIgnoreCase("s")) {
+                                System.out.println("Compra realizada con éxito.");
+                            }
+                        } else {
+                            System.out.println("Debe seleccionar un curso.");
+                        }
                     }
-                }
-            }
+                    break;
 
-            else if (opcion == 1 && sesionIniciada && !esAdmin) {
-                System.out.println("1. " + titulo1 + " - $" + precio1);
-                System.out.println("2. " + titulo2 + " - $" + precio2);
-                System.out.println("3. " + titulo3 + " - $" + precio3);
+                case 3:
+                    if (admin) {
+                        for (int i = 0; i < totalUsuarios; i++) {
+                            System.out.println((i + 1) + ". " + nombres[i]);
+                        }
 
-                System.out.print("Seleccione un curso: ");
-                elegir = sc.nextInt();
-                sc.nextLine();
+                        System.out.print("Usuario a eliminar: ");
+                        int u = sc.nextInt();
+                        sc.nextLine();
 
-                if (elegir == 1) cursos[usuarioActual] = titulo1;
-                else if (elegir == 2) cursos[usuarioActual] = titulo2;
-                else if (elegir == 3) cursos[usuarioActual] = titulo3;
+                        if (u > 0 && u <= totalUsuarios) {
+                            for (int i = u - 1; i < totalUsuarios - 1; i++) {
+                                nombres[i] = nombres[i + 1];
+                                correos[i] = correos[i + 1];
+                                claves[i] = claves[i + 1];
+                                cursosUsuario[i] = cursosUsuario[i + 1];
+                            }
+                            totalUsuarios--;
+                            System.out.println("Usuario eliminado.");
+                        }
+                    } else {
+                        System.out.println("Nombre: " + nombres[usuarioActual]);
+                        System.out.println("Correo: " + correos[usuarioActual]);
+                        System.out.println("Curso: " + cursosUsuario[usuarioActual]);
+                    }
+                    break;
 
-                System.out.println("Curso seleccionado: " + cursos[usuarioActual]);
-            }
+                case 4:
+                    if (admin) {
+                        System.out.print("Nombre del curso: ");
+                        cursos[totalCursos] = sc.nextLine();
+                        System.out.print("Precio: ");
+                        precios[totalCursos] = sc.nextDouble();
+                        sc.nextLine();
+                        totalCursos++;
+                        System.out.println("Curso agregado.");
+                    } else {
+                        sesion = false;
+                        usuarioActual = -1;
+                        System.out.println("Sesión cerrada.");
+                    }
+                    break;
 
-            else if (opcion == 2 && sesionIniciada && !esAdmin) {
-                if (!cursos[usuarioActual].equals("Ninguno")) {
-                    System.out.print("¿Desea comprar el curso? (s/n): ");
+                case 5:
+                    if (admin) {
+                        for (int i = 0; i < totalCursos; i++) {
+                            System.out.println((i + 1) + ". " + cursos[i]);
+                        }
+
+                        System.out.print("Curso a eliminar: ");
+                        int c = sc.nextInt();
+                        sc.nextLine();
+
+                        if (c > 0 && c <= totalCursos) {
+                            for (int i = c - 1; i < totalCursos - 1; i++) {
+                                cursos[i] = cursos[i + 1];
+                                precios[i] = precios[i + 1];
+                            }
+                            totalCursos--;
+                            System.out.println("Curso eliminado.");
+                        }
+                    }
+                    break;
+
+                case 6:
+                    if (admin) {
+                        sesion = false;
+                        admin = false;
+                        System.out.println("Sesión cerrada.");
+                    }
+                    break;
+
+                case 0:
+                    System.out.print("¿Desea salir? (s/n): ");
                     r = sc.nextLine();
-                    if (r.equalsIgnoreCase("s")) {
-                        System.out.println("Compra realizada con éxito.");
-                    }
-                } else {
-                    System.out.println("Debe seleccionar un curso.");
-                }
-            }
-
-            else if (opcion == 3 && sesionIniciada && !esAdmin) {
-                System.out.println("Nombre: " + nombres[usuarioActual]);
-                System.out.println("Correo: " + correos[usuarioActual]);
-                System.out.println("Curso: " + cursos[usuarioActual]);
-            }
-
-            else if (opcion == 1 && esAdmin) {
-                for (int i = 0; i < contadorUsuarios; i++) {
-                    System.out.println((i + 1) + ". " + nombres[i] + " | " + correos[i] + " | " + cursos[i]);
-                }
-            }
-
-            else if (opcion == 2 && esAdmin) {
-                System.out.print("Nuevo precio Desarrollo Web: ");
-                precio1 = sc.nextDouble();
-                System.out.print("Nuevo precio JavaScript: ");
-                precio2 = sc.nextDouble();
-                System.out.print("Nuevo precio CSS: ");
-                precio3 = sc.nextDouble();
-                sc.nextLine();
-                System.out.println("Precios actualizados.");
-            }
-
-            else if (opcion == 3 && esAdmin) {
-                for (int i = 0; i < contadorUsuarios; i++) {
-                    System.out.println((i + 1) + ". " + nombres[i]);
-                }
-                System.out.print("Seleccione el usuario a borrar: ");
-                borrar = sc.nextInt();
-                sc.nextLine();
-
-                if (borrar >= 1 && borrar <= contadorUsuarios) {
-                    for (int i = borrar - 1; i < contadorUsuarios - 1; i++) {
-                        nombres[i] = nombres[i + 1];
-                        correos[i] = correos[i + 1];
-                        contrasenas[i] = contrasenas[i + 1];
-                        cursos[i] = cursos[i + 1];
-                    }
-                    contadorUsuarios--;
-                    System.out.println("Usuario eliminado.");
-                }
-            }
-
-            else if (opcion == 4 && sesionIniciada) {
-                sesionIniciada = false;
-                esAdmin = false;
-                usuarioActual = -1;
-                System.out.println("Sesión cerrada.");
-            }
-
-            else if (opcion == 0) {
-                System.out.print("¿Desea salir? (s/n): ");
-                salir = sc.nextLine();
-                if (!salir.equalsIgnoreCase("s")) {
-                    opcion = -1;
-                } else {
-                    System.out.println("Gracias por usar la plataforma.");
-                }
+                    if (!r.equalsIgnoreCase("s")) opcion = -1;
+                    break;
             }
 
         } while (opcion != 0);
@@ -194,4 +234,5 @@ public class Main {
         sc.close();
     }
 }
+
 
